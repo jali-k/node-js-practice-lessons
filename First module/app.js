6e8b8643807a1ef12ffd7faff214b5a63655cdbc
jali-k <e19210@eng.pdn.ac.lk>
2023-09-13 16:48:53 +0530

    First module

---
+++ First module/app.js
@@ -0,0 +1,14 @@
+// This is the nodejs app we are running and we import other files properties somehow.
+
+// We need to import here the things that we exported
+
+// That can be done with require global
+
+const names = module.require("./people")
+const sayHi = module.require("./talk")
+
+console.log(names);
+console.log(sayHi);
+
+sayHi(names.john); // "Hi john"
+sayHi(names.peter); // "Hi peter"
---
+++ First module/people.js
@@ -0,0 +1,9 @@
+const secretPerson = 'SECRET PERSON' // We are not sharing him
+
+// we share these two people
+const john = "john"
+const peter = "peter"
+
+// Every js file is a module
+
+module.exports = {john, peter} // Exporting people "exports"
