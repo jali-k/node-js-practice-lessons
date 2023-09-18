cc4645debb272f70dc35c8a83a0d1fdae67b03c8
jali-k <e19210@eng.pdn.ac.lk>
2023-09-18 18:16:03 +0530

    Setting up routes

--- 21.Exspress methods/app.js
+++ 21.Exspress methods/app.js
@@ -6,8 +6,14 @@ app.use(exspress.static('./public'))
 
 const {countries} = require('./data');
 
+
+
+const countriesMiddleware = require('./routes/countries')
+
 app.use(exspress.urlencoded({extended: false})) // handle POST requests body. Handle data in the trype "application/x-www-form-urlencoded"
 app.use(exspress.json()); // Handle the data in the type "application/json"
+
+app.use("/change/",countriesMiddleware);
 
 app.get("/", (req, res) => {
     res.status(200).json({success:true})
@@ -23,53 +29,7 @@ app.post("/login", (req, res)=>{
     }else{
        return res.status(401).send("Please enter valid credentials")
     }
-})
-
-
-app.put("/change/:id", (req, res)=>{
-   
-    const {id} = req.params;
-    const {title} = req.body;
-    
-    console.log(id);
-    console.log(req.body);
-    if (id) {
-
-        const selectedCountry = countries.find((country) => country.id == Number(id));
-
-        selectedCountry.title = title
-
-        return res.status(200).send(selectedCountry)
-    }
-    res.status(401).send("No id there");
-})
-
-
-app.delete("/change/:id", (req, res)=>{
-   
-    const {id} = req.params;
-
-    
-    console.log(id);
-
-    if (id) {
-
-        const selectedCountry = countries.find((country) => country.id == Number(id));
-
-        if (!selectedCountry) {
-            return res.status(404).send(`No country with that id: ${id}`)
-        }
-
-        const newCountries = countries.filter((country) => country.id !== Number(id));
-
-    
-
-        return res.status(200).json(newCountries);
-    }
-    res.status(401).send("No id there");
-})
-
-
+});
 
 app.listen(5000, ()=>{
     console.log("Server started and running on port 5000");
---
+++ 21.Exspress methods/routes/countries.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {countries} = require('../data');
+
+
+
+router.put("/:id", (req, res)=>{
+   
+    const {id} = req.params;
+    const {title} = req.body;
+    
+    console.log(id);
+    console.log(req.body);
+    if (id) {
+
+        const selectedCountry = countries.find((country) => country.id == Number(id));
+
+        selectedCountry.title = title
+
+        return res.status(200).send(selectedCountry)
+    }
+    res.status(401).send("No id there");
+})
+
+
+router.delete("/:id", (req, res)=>{
+   
+    const {id} = req.params;
+
+    
+    console.log(id);
+
+    if (id) {
+
+        const selectedCountry = countries.find((country) => country.id == Number(id));
+
+        if (!selectedCountry) {
+            return res.status(404).send(`No country with that id: ${id}`)
+        }
+
+        const newCountries = countries.filter((country) => country.id !== Number(id));
+
+    
+
+        return res.status(200).json(newCountries);
+    }
+    res.status(401).send("No id there");
+})
+
+module.exports = router;
